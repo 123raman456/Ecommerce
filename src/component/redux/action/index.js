49bdd04455623dfca7308ed70fb050638863ff32
Raman Verma <123955950+123raman456@users.noreply.github.com>
2023-05-05 15:11:26 +0530

    Create index.js

---
+++ src/component/redux/action/index.js
@@ -0,0 +1,16 @@
+// to add item to cart
+
+export const addCart = (product)=>{
+    return{
+        type:"ADDITEM",
+        payload: product
+    }
+}
+// to delete item from cart
+
+export const delCart = (product)=>{
+    return{
+        type:"DELITEM",
+        payload: product
+    }
+}
